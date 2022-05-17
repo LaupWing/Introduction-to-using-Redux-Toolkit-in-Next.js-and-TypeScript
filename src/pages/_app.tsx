a68655c2600daedb4f4bde5cd24f1e430fe4e039
LaupWing <loc_strict@hotmail.com>
2022-05-17 19:41:30 +0200

    _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import {Provider} from 'react-redux'
+import type { AppProps } from 'next/app'
+import { store } from '../../store'
+
+
+function MyApp({
+   Component,
+   pageProps
+}:AppProps){
+   return (
+      <Provider store={store}>
+         <Component {...pageProps} />
+      </Provider>
+   )
+}
