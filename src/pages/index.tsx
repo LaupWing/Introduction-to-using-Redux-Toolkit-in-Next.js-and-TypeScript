5c71faa90011b9462da98f78c5eca35c09f15833
LaupWing <loc_strict@hotmail.com>
2022-05-18 10:47:00 +0200

    hmm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAppDispatch, useAppSelector } from '../app/hook'
+import { decrement, increment, incrementByAmount, selectCount } from '../features/counter/counterSlice'
 
-const Home = () => {
+const IndexPage:FC = () => {
+   const dispatch = useAppDispatch
+   const count = useAppSelector(selectCount)
+   const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
    return (
-      <div>Home</div>
+      <>
+         <h1>Welcome to the greatest app in the world</h1>
+         <h2>The current number is {count}</h2>
+         <div>
+            <input 
+               type="number" 
+               onChange={e=> setIncrementAmount(Number(e.target.value))}
+               value={incrementAmount}
+            />
+            <button
+               onClick={()=> dispatch(incrementByAmount(Number(incrementAmount)))}
+            >
+
+            </button>
+         </div>
+         <div>
+            <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+            <button onClick={() => dispatch(increment())}>Increment by 1</button>
+         </div>
+      </>
    )
 }
 
-export default Home
+export default IndexPage
